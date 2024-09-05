eb9310fb45a919cea92a0b2dc6751c4e192aff22
J7NU <J7NU>
2024-09-05 17:16:22 +0900

    이것도 1단 동기화를 해봐

---
+++ 1929.py
@@ -0,0 +1,13 @@
+import time
+import sys
+start_time = time.time()
+
+# ------------------------------------------------------
+
+MN_list = sys.stdin.readline().rstrip()
+# MN_list 0보다 크고 1보다 작은 소수를 모두 출력해야함
+
+# ------------------------------------------------------
+end_time = time.time()
+
+print(f'코드 실행 시간: {end_time - start_time:.4f}')
